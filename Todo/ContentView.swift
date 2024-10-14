88693ddf15a868bb46fd89d0196c089410f8157f
Berkan Türkali <berkanturkali@outlook.com>
2024-10-14 07:03:29 +0300

    add custom font family

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -6,6 +6,18 @@ import SwiftData
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
+            .font(.custom(Typeface.regular, size: 18))
+
+    }
+    
+    init() {
+        for familyName in UIFont.familyNames {
+            print(familyName)
+            
+            for fontName in UIFont.fontNames(forFamilyName: familyName) {
+                print("--  \(fontName)")
+            }
+        }
     }
 }
 
---
+++ Todo/Utils/Typeface.swift
@@ -0,0 +1,44 @@
+//
+
+import Foundation
+
+
+struct Typeface {    
+    
+    static var extraLight: String {
+        "Exo2-ExtraLight"
+    }
+    
+    static var light: String {
+        "Exo2-Light"
+    }
+    
+    static var thin: String {
+        "Exo2-Thin"
+    }
+    
+    static var regular: String {
+        "Exo2-Regular"
+    }
+    
+    static var medium: String {
+        "Exo2-Medium"
+    }
+    
+    static var semibold: String {
+        "Exo2-SemiBold"
+    }
+    
+    static var bold: String {
+        "Exo2-Bold"
+    }
+    
+    static var black: String {
+        "Exo2-Black"
+    }
+    
+    static var extraBold: String {
+        "Exo2-ExtraBold"
+    }
+    
+}
