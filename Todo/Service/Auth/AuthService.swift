b19c8cd70f296d05d262e9fd41c47d6aec69048f
Berkan Türkali <berkanturkali@outlook.com>
2024-10-19 18:01:39 +0300

    add reusable generic network request function

--- Todo/Service/Auth/AuthService.swift
+++ Todo/Service/Auth/AuthService.swift
@@ -21,70 +21,20 @@ struct AuthService {
         
         let endpoint = AuthServiceEndpoints.signup
         
-        print(endpoint)
-        
-        guard let url = URL(string: endpoint) else {
-            throw NetworkError.badURL(endpoint)
-        }
-        
         do {
-            
-            let jsonBody = try JSONEncoder().encode(body)
-            
-            var request = URLRequest(url: url)
-            
-            request.httpMethod = HttpMethod.POST.rawValue
-            
-            request.httpBody = jsonBody
-            
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-            
-            let (data, response) = try await URLSession.shared.data(for: request)
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? 500
-                if let errorMessage = extractErrorMessage(from: data) {
-                    throw NetworkError.httpError(statusCode: statusCode, localizedDesc: errorMessage)
-                } else {
-                    throw NetworkError.httpError(statusCode: statusCode, localizedDesc: LocalizedStrings.somethingWentWrong)
-                }
-            }
-            
-            let decodedResponse = try JSONDecoder().decode(BaseResponse<NoData>.self, from: data)
-            
-            return decodedResponse.message
-            
-        } catch let error {
-            switch error {
-            case let urlError as URLError:
-                switch urlError.code {
-                case .notConnectedToInternet:
-                    throw NetworkError.notConnectedToInternet
-                case .timedOut:
-                    throw NetworkError.timedOut
-                case .cannotFindHost:
-                    throw NetworkError.cannotConnectToHost(endpoint)
-                default:
-                    throw NetworkError.requestFailed(error.localizedDescription)
-                }
                 
-            case let decodingError as DecodingError:
-                throw NetworkError.decodingError(decodingError)
-            default:
-                throw NetworkError.unknownError
-            }
+            let response: BaseResponse<NoData> = try await NetworkManager.shared.request(
+                to: endpoint,
+                method: HttpMethod.POST,
+                body: body,
+                responseType: BaseResponse<NoData>.self
+            )
+            return response.message
             
-        }
-        
-    }
-    
-    func extractErrorMessage(from data: Data) -> String? {
-        do {
-            let errorResponse = try JSONDecoder().decode(ErrorResponse.self, from: data)
-            return errorResponse.message
-        } catch {
-            return nil
+        } catch  {
+                
+            throw error
         }
     }
+
 }
--- Todo/Service/NetworkManager.swift
+++ Todo/Service/NetworkManager.swift
@@ -8,7 +8,7 @@ struct NetworkManager {
     
     static let shared = NetworkManager()
     
-    private init() {}    
+    private init() {}
     
     func handleNetworkError(_ error : NetworkError) -> String {
         switch error {
@@ -28,6 +28,75 @@ struct NetworkManager {
             return String(format: LocalizedStrings.canNotConnectedToHost, host)
         case .unknownError:
             return LocalizedStrings.somethingWentWrong
+        }
+    }
+    
+    func request<T: Encodable, U: Codable> (
+        to endpoint: String,
+        method: HttpMethod,
+        body: T? = nil,
+        responseType: U.Type
+    ) async throws -> U {
+        
+        
+        guard let url = URL(string: endpoint) else {
+            throw NetworkError.badURL(endpoint)
+        }
+        
+        do {
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = method.rawValue
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            if let body = body {
+                let jsonBody = try JSONEncoder().encode(body)
+                request.httpBody = jsonBody
+            }
+            
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? 500
+                if let errorMessage = extractErrorMessage(from: data) {
+                    throw NetworkError.httpError(statusCode: statusCode, localizedDesc: errorMessage)
+                } else {
+                    throw NetworkError.httpError(statusCode: statusCode, localizedDesc: LocalizedStrings.somethingWentWrong)
+                }
+            }
+            
+            let decodedResponse = try JSONDecoder().decode(responseType, from: data)
+            return decodedResponse
+            
+            
+        } catch let error {
+            switch error {
+            case let urlError as URLError:
+                switch urlError.code {
+                case .notConnectedToInternet:
+                    throw NetworkError.notConnectedToInternet
+                case .timedOut:
+                    throw NetworkError.timedOut
+                case .cannotFindHost:
+                    throw NetworkError.cannotConnectToHost(endpoint)
+                default:
+                    throw NetworkError.requestFailed(error.localizedDescription)
+                }
+                
+            case let decodingError as DecodingError:
+                throw NetworkError.decodingError(decodingError)
+            default:
+                throw NetworkError.unknownError
+            }
+        }
+    }
+    
+    private func extractErrorMessage(from data: Data) -> String? {
+        do {
+            let errorResponse = try JSONDecoder().decode(ErrorResponse.self, from: data)
+            return errorResponse.message
+        } catch {
+            return nil
         }
     }
 }
